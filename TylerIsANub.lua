2e82d95100beae76b7d0058a552fa291d80b52c1
Jason L Perry <jasper@ambethia.com>
2010-07-24 17:50:02 -0400

    Add support for Warlock buffs.

--- TylerIsANub.lua
+++ TylerIsANub.lua
@@ -4,7 +4,7 @@ f:RegisterEvent("PLAYER_LOGIN");
 
 function f:PLAYER_LOGIN(...)
   local class = select(2, UnitClass("player"));
-  if class == "ROGUE" or class == "SHAMAN" then f:RegisterEvent("READY_CHECK"); end
+  if class == "ROGUE" or class == "SHAMAN" or class == "WARLOCK" then f:RegisterEvent("READY_CHECK"); end
   f:UnregisterEvent("PLAYER_LOGIN"); self.PLAYER_LOGIN = nil;
 end
 
@@ -13,7 +13,7 @@ function f:READY_CHECK(...)
   local mH, mHD, _, oH, oHD = GetWeaponEnchantInfo();
   local oHT = select(7, GetItemInfo(GetInventoryItemLink("player", GetInventorySlotInfo("SecondaryHandSlot")) or ""));
   if (mH ~= 1 or ((mHD or 0) <= 300000)) or ((oH ~= 1 or ((mHD or 0) <= 300000)) and oHT ~= "Shields") then
-    StaticPopup_Show("TYLER_IS_A_NUB", class == "ROGUE" and "Poisons" or "Weapon Buffs");
+    StaticPopup_Show("TYLER_IS_A_NUB", class == "ROGUE" and "Poisons" or "Weapon Buff(s)");
   end
 end
 
